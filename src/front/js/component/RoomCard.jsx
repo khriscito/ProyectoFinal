4ae793b5b72dbc3434047424c4a70e7342905b01
olara <octavioasapchi@gmail.com>
2023-05-15 20:19:13 +0000

    [:sparkles: UPDATE] dashboard integration

--- src/front/js/component/RoomCard.jsx
+++ src/front/js/component/RoomCard.jsx
@@ -1,11 +1,27 @@
-import React, { useState, createContext } from "react";
-import  ReactDOM  from 'react-dom';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
 
-const RoomCard = () => {
-    return (
-        
-    )
-}
+const variants = {
+  primary: "bg-primary",
+  warning: "bg-warning",
+  success: "bg-success",
+  danger: "bg-danger",
+};
+const RoomCard = ({ variant = "primary", roomNumber = "43" }) => {
+  return (
+    <div className="col-xl-3 col-md-6">
+      <div className={`card ${variants[variant]} text-white mb-4`}>
+        <div className="card-body">Habitación {roomNumber}</div>
+        <div className="card-footer d-flex align-items-center justify-content-between">
+          <a className="small text-white stretched-link" href="#">
+            View Details
+          </a>
+          <div className="small text-white">
+            <i className="fas fa-angle-right"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default RoomCard
+export default RoomCard;
